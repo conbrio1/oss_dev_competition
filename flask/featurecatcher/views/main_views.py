13b0285f81f537110852b3e7c553a60b21e778f2
conbrio1 <asdafe45@gmail.com>
2020-10-17 16:45:26 +0900

    developing flask-web-ui

---
+++ flask/featurecatcher/views/main_views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, url_for, current_app
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
